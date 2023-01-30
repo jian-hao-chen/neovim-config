9a6df390548216d7639075e8aead3afb34970d0e
Jian-Hao Chen <billchen85@gmail.com>
2023-01-30 16:47:09 +0800

    Add files

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ My Neovim configuration.
 ## Usage
 
 ### Windows
+The following command need to be executed in `cmd.exe`.
 ```shell
-git clone url %LOCALAPPDATA%/nvim
+git clone https://github.com/jian-hao-chen/neovim-config.git %LOCALAPPDATA%\nvim
+```
+
+### Unix
+```shell
+git clone https://github.com/jian-hao-chen/neovim-config.git ~/.config/nvim
 ```
---
+++ lua/user/editor.lua
@@ -0,0 +1,39 @@
+-- 啟用滑鼠
+vim.opt.mouse = 'a'
+
+-- 設定檔案編碼格式
+vim.opt.encoding = 'utf-8'
+
+
+-- === 行號相關設定 ===
+-- 1. 顯示行號
+vim.opt.number = true
+
+-- 2. 顯示相對行號
+vim.opt.relativenumber = false
+
+-- 3. 總是顯示標號欄
+vim.opt.signcolumn = 'yes'
+
+
+-- === 搜尋相關設定 ===
+-- 1. 搜尋時忽略大小寫
+vim.opt.ignorecase = true
+
+-- 2. 若搜尋模式出現大寫則區分大小寫
+vim.opt.smartcase = true
+
+-- 3. 輸入搜尋模式同時高亮部分匹配
+vim.opt.incsearch = true
+
+
+-- === Tab 相關設定 ===
+-- 1. 讀取到 Tab 字元時顯示的寬度
+vim.opt.tabstop = 8
+
+-- 2. 縮排時使用的寬度
+vim.opt.shiftwidth = 2
+
+-- 3. 是否以空格代替 Tab 字元
+vim.opt.expandtab = true
+
