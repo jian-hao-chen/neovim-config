91fe77f9186a00532fe0394d9c163b3b9592e2a6
Jian-Hao Chen <billchen85@gmail.com>
2023-01-31 11:58:10 +0800

    Add autocmd to restore cursor style

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -1,6 +1,14 @@
+-- 退出 Neovim 後回復游標樣式
+vim.api.nvim_create_autocmd({"VimLeave", "VimSuspend"}, {
+  desc = "Restore cursor style after leaving Neovim.",
+  pattern = "*",
+  command = "set guicursor=a:ver25-blinkwait700-blinkoff400-blinkon250-Cursor"
+})
+
+
 -- 複製時高亮複製的內容
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight selection on yank",
+  desc = "Highlight selection on yank.",
   pattern = "*",
   callback = function()
     vim.highlight.on_yank({
