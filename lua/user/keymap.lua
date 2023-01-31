49a1faa2566c0ffee12030862351b9b4f37043e9
Jian-Hao Chen <billchen85@gmail.com>
2023-01-31 16:54:15 +0800

    Add keymaps

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -1,6 +1,7 @@
 -- 設定 Leader 鍵為空白鍵
 vim.g.mapleader = " "
 
+
 -- 設定格式:
 -- vim.keymap.set({mode}, {lhs}, {rhs}, {opts})
 --   * {mode} [string|table]    映射生效的模式
@@ -8,6 +9,9 @@ vim.g.mapleader = " "
 --   * {rhs}  [string|function] 觸發後執行的功能，可以是按鍵、函式、或其他內建功能
 --   * {opts} [table|nil]       其他設定選項
 
+
+-- 按空白件時不要移動游標
+vim.keymap.set({ "n", "v" }, "<space>", "<nop>")
 
 -- 捲動畫面後游標置中
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -19,4 +23,14 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 -- 選取並覆蓋內容時保留緩衝區內的內容
 vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- 複製到系統剪貼簿
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>Y", [["+Y]])
+
+-- 刪除內容且不要存放到緩衝區
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>D", [["_D]])
+
+
 
