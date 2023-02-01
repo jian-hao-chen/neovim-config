1539f8df3925be1be7c56f99f1056787642b8099
Jian-Hao Chen <billchen85@gmail.com>
2023-02-01 22:35:22 +0800

    Change some colors

--- lua/user/appearance.lua
+++ lua/user/appearance.lua
@@ -1,17 +1,23 @@
 -- 顯示更多顏色
 vim.opt.termguicolors = true
 
--- 設定文字寬度
--- vim.opt.textwidth = 80
+-- 設定唯一狀態列
+vim.opt.laststatus = 3
 
--- 超過設定的文字寬度改變背景顏色
--- vim.opt.colorcolumn = "+0"
+-- 不顯示預設的模式文字
+vim.opt.showmode = false
+
+-- 設定高亮當前行
+vim.opt.cursorline = true
+
+-- 設定緩衝區結束字元為空白
+vim.opt.fillchars = "eob: "
 
 -- 設定顏色
 local colors = {
     bg = "#22212C",
     fg = "#F8F8F2",
-    selection = "#7970A9",
+    selection = "#454158",
     comment = "#7970A9",
     red = "#FF9580",
     orange = "#FFCA80",
@@ -27,18 +33,29 @@ local colors = {
     bright_magenta = "#FF99CC",
     bright_cyan = "#99FFEE",
     bright_white = "#FFFFFF",
-    menu = "#FFFFFF",
+    menu = "#151320",
     visual = "#454158",
     gutter_fg = "#F8F8F2",
-    nontext = "#3B4048",
-    white = "#FFFFFF",
-    black = "#0B0A10",
+    nontext = "#7970A9",
+    white = "#F8F8F2",
+    black = "#151320",
 }
 
 -- 設定主題 (注意: 不同主題載入方法可能不一樣)
 local theme = require("dracula")
 theme.setup({
   colors = colors,
-  lualine_bg_color = "#151320",
+  lualine_bg_color = colors.menu,
 })
 vim.cmd("colorscheme dracula")
+
+
+-- 設定游標所在列的行號顏色
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = colors.orange })
+
+-- 設定緩衝區分隔線顏色
+vim.api.nvim_set_hl(0, "VertSplit", {fg = colors.visual })
+
+-- 重新指定 nvim-tree.nvim 背景顏色 
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = colors.bg })
+
